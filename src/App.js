65cb311490b35084f717e23325b957fa245a928c
costelmarianmereuta <mereuta.mariana1@gmail.com>
2020-11-10 12:53:23 +0100

    add dockerfile

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hi there
+          HI THERE
         </p>
         <a
           className="App-link"
